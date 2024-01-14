88db0ae3b8f5437679c831e56ba3a97df1a89b71
Heet Vakharia <heetkv@gmail.com>
2024-01-14 21:34:27 +0530

    Added Docker

--- src/utils/initialSetup.ts
+++ src/utils/initialSetup.ts
@@ -2,7 +2,7 @@ import mysql from 'mysql';
 
 const init = () => { 
     const connection = mysql.createConnection({
-        // host: process.env.DB_HOST,
+        host: process.env.DB_HOST,
         port: parseInt(process.env.DB_PORT || '3306'),
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
