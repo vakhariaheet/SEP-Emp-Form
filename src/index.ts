b4c8f8ef9f2fd381f811e0f8d5d69b179c22f0b2
Heet Vakharia <heetkv@gmail.com>
2024-01-22 10:38:13 +0530

    Added Get Route

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,25 @@ app.use(express.json());
 dotenv.config();
 
 const db = init();
+app.get("/", (req, res) => { 
+    res.send("Server running");
+})
 
 app.post("/api/add", async (req, res) => { 
     const { name, salary, date, position } = req.body;
     if(!name || !salary || !date || !position) return res.status(400).json({ message: "Please fill all the fields", isSuccess: false });
     const id = crypto.randomUUID();
-    await db.query(`INSERT INTO employees (id,name, salary, join_date, position) VALUES ('${id}','${name}', ${salary}, '${date}', '${position}')`);
-    res.status(200).json({ message: "Employee added",isSuccess: true });
+    db.query(`INSERT INTO employees (id,name, salary, join_date, position) VALUES ('${id}','${name}', ${salary}, '${date}', '${position}')`, (err, result) => { 
+        if (err) return res.status(500).json({ message: "Something went wrong", isSuccess: false, err });
+        res.status(200).json({ message: "Employee added",isSuccess: true });
+    });
 })
 
 app.get("/get", async (req, res) => {
-    const result = await db.query("SELECT * FROM employees");
-    res.status(200).json({ data: result, isSuccess: true });
+   db.query("SELECT * FROM employees", (err, result) => { 
+       if (err) return res.status(500).json({ message: "Something went wrong", isSuccess: false,err });
+        res.status(200).json({ data: result, isSuccess: true });
+    });
 });
 
 const PORT = process.env.PORT || 3004;
